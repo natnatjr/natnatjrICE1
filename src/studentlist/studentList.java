c33c093d51ea2cb5aaa3a36ed075f15609e5d7b5
natnatjr <ejnatnat@yahoo.com>
2022-02-02 17:05:45 -0500

    studentList [2] array name changed to "no"

--- src/studentlist/studentList.java
+++ src/studentlist/studentList.java
@@ -9,16 +9,18 @@ package studentlist;
  *
  * @author Ronak
  */
-public class studentList {
-    public static void main(String[] args){
-        Student[] studentList = new Student[3];
-        studentList[0] = new Student("1","Ronak Sheth");
-        studentList[1] = new Student("2","John Doe");
-        studentList[2] = new Student("3","abc");
-        
-        for(int  i = 0;i<studentList.length;i++){
-            System.out.println(studentList[i].getStudentName());
-        }
-        
-    }
+public class studentList
+{
+   public static void main (String[] args)
+   {
+      Student[] studentList = new Student[3];
+      studentList[0] = new Student("1", "Ronak Sheth");
+      studentList[1] = new Student("2", "John Doe");
+      studentList[2] = new Student("3", "no");
+
+      for (int i = 0; i < studentList.length; i++) {
+         System.out.println(studentList[i].getStudentName());
+      }
+
+   }
 }
